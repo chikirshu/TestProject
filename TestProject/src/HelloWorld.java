cfae63e04e697417b1f3c062f2d94b0f749f1668
kappa10 <37349638+kappa10@users.noreply.github.com>
2018-03-31 16:48:59 +0530

    Committed from site

--- TestProject/src/HelloWorld.java
+++ TestProject/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Shashank!!");
-		System.out.println("Lets start");
+		System.out.println("Lets start!!");
 		
 		System.out.println("OK");
 		
